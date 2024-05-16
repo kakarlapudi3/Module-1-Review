6691cffa6dbacc7561463956b093539c6d9ffeb5
Ria Kakarlapudi <ria.kakarlapudi@gmail.com>
2024-05-16 14:47:07 -0500

    initial commit

--- Desktop/Repos/Module 1 Review/Module 1 Review/obj/Debug/net8.0/Module 1 Review.AssemblyInfo.cs
+++ Desktop/Repos/Module 1 Review/Module 1 Review/obj/Debug/net8.0/Module 1 Review.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module 1 Review")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16064439d82f324f341a2d89e81c43134c22d076")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module 1 Review")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module 1 Review")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
